07a9e16bbc2b60987d1c033433c21c17ca4d6d0c
daipenger <fobbes@web.de>
2016-04-05 16:57:38 +0200

    Cleaning up util.reflection

--- src/main/java/info/ata4/minecraft/minema/util/reflection/PrivateFields.java
+++ src/main/java/info/ata4/minecraft/minema/util/reflection/PrivateFields.java
@@ -14,17 +14,9 @@ package info.ata4.minecraft.minema.util.reflection;
  *
  * @author Nico Bergemann <barracuda415 at yahoo.de>
  */
-public interface PrivateFields {
+public final class PrivateFields {
 
 	public static final String[] TIMER_TICKSPERSECOND = new String[] { "ticksPerSecond", "field_74282_a" };
 	public static final String[] MINECRAFT_TIMER = new String[] { "timer", "field_71428_T" };
-	public static final String[] MINECRAFT_STREAM = new String[] { "stream", "field_152353_at" };
-	public static final String[] MINECRAFT_EFFECTRENDERER = new String[] { "effectRenderer", "field_71452_i" };
-	public static final String[] FRAMEBUFFER_FRAMEBUFFERTEXTUREWIDTH = new String[] { "framebufferTextureWidth",
-			"field_147622_a" };
-	public static final String[] FRAMEBUFFER_FRAMEBUFFERTEXTUREHEIGHT = new String[] { "framebufferTextureHeight",
-			"field_147620_b" };
-	public static final String[] EFFECTRENDERER_FXLAYERS = new String[] { "fxLayers", "field_78876_b" };
-	public static final String[] RENDERGLOBAL_CHUNKSTOUPDATE = new String[] { "chunksToUpdate", "field_175009_l" };
-	public static final String[] RENDERGLOBAL_RENDERDISPATCHER = new String[] { "renderDispatcher", "field_178524_h" };
+
 }
